cb3977f2f6eb261395920be146200d0751d5b9bd
colinthomas-z80 <cthomas0687@gmail.com>
2020-12-25 21:47:20 -0500

    Remove an include, add some comments

--- main/main.c
+++ main/main.c
@@ -1,9 +1,13 @@
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "sdkconfig.h"
 #include "display.h"
- 
+
+/*
+    Create a struct of the DISPLAY_PINS type, then call display_select_gpio on it.
+    Each number argument corresponds to segment A, B, C, D, E, F, G respectively.
+    You can see which segments these are on the datasheet. 
+ */
 
 void app_main(void)
 {
